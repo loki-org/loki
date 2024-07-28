7680eef83f431b5fcd3b97e0fee3b36d3343dfbb
Lukas Neubert <lukas.neubert@proton.me>
2024-07-28 22:39:50 +0200

    gen: py

--- cli/backends/py.js
+++ cli/backends/py.js
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 import { BaseGen } from '../gen.js'
+import { IDXS } from '../table.js'
 
 class Gen extends BaseGen {
 	setup() {
@@ -14,8 +15,39 @@ class Gen extends BaseGen {
 		this.writeln('')
 	}
 
+	fun_decl(node) {
+		this.write(`def ${node.name}(`)
+		for (let i = 0; i < node.params.length; i++) {
+			const param = node.params[i]
+			this.write(`${param.name}: ${this.type(param.type)}`)
+			if (i < node.params.length - 1) {
+				this.write(', ')
+			}
+		}
+		this.writeln(`) -> ${this.type(node.return_type)}:`)
+		this.stmts(node.body)
+		if (node.body.length === 0) {
+			this.indent++
+			this.writeln('pass')
+			this.indent--
+		}
+	}
+
 	cast_expr(node) {
 		this.expr(node.expr)
+	}
+
+	type(t) {
+		switch(t) {
+			case IDXS.void:
+				return 'void'
+			case IDXS.i32:
+				return 'int'
+			case IDXS.u32:
+				return 'int'
+			default:
+				return 'void*'
+		}
 	}
 }
 
