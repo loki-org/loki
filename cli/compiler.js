8cac996de8ae4e7bd5bb43c0dc1f723f9d61b824
Lukas Neubert <lukas.neubert@proton.me>
2024-09-22 12:33:43 +0200

    loki: add --outdir option and default to `build` directory (#18)

--- cli/compiler.js
+++ cli/compiler.js
@@ -1,7 +1,8 @@
 // SPDX-FileCopyrightText: 2024-present Lukas Neubert <lukas.neubert@proton.me>
 // SPDX-License-Identifier: MPL-2.0
 
-import * as fs from 'fs'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 import { Table } from "./table.js"
 import { parse } from './parser.js'
 import { Sema } from './sema.js'
@@ -15,13 +16,18 @@ function compile(prefs) {
 	const sema = new Sema(table)
 	sema.check(ast)
 
+	const file_name = path.basename(prefs.file)
+	const out_path_lo = path.join(prefs.options.outdir, file_name)
+
 	for (const backend of prefs.options.backends) {
 		gen(ast, table, backend).then((out) => {
-			const out_path = prefs.file.replace('.lo', `.${backend}`)
+			const out_path = out_path_lo.replace('.lo', `.${backend}`)
+			fs.mkdirSync(prefs.options.outdir, { recursive: true })
 			fs.writeFileSync(out_path, out.main)
 
-			if (!out.alt_path.endsWith('/')) {
-				fs.writeFileSync(out.alt_path, out.alt)
+			if (out.alt_name.length > 0) {
+				const alt_path = path.join(prefs.options.outdir, out.alt_name)
+				fs.writeFileSync(alt_path, out.alt)
 			}
 		})
 	}
